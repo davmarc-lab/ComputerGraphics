7af7cf164d0ca9685e354b2bf278259d55c55a4c
Davide Marchetti <davide.marchetti6@studio.unio.it>
2023-10-19 16:13:25 +0200

    trying to mix utils and window but got segmentation fault on createVertexArray. Solution explained in comments in Utils.cpp

--- Forbidden/SimpleWindow/Utils/Utils.cpp
+++ Forbidden/SimpleWindow/Utils/Utils.cpp
@@ -1,32 +1,51 @@
 #include "Utils.hpp"
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-Shape2D::Shape2D(vector<vec3> vertex, vector<vec4> colors, mat4 model){
+using namespace std;
+
+Shape2D::Shape2D(int ntriangle, vector<vec3> vertex, vector<vec4> colors, mat4 model)
+{
+    this->ntriangle = ntriangle;
     this->vertex = vertex;
     this->colors = colors;
     this->model = model;
+    this->nvertex = this->vertex.size();
+}
+
+Shape2D::Shape2D(int ntriangle)
+{
+    this->ntriangle = ntriangle;
 }
 
 /* Creates the VertexarrayObject and assign to the VertexBufferObject the vertex and colors values.
  */
 void Shape2D::createVertexArray(int n)
 {
-    glGenVertexArrays(n, &this->vao);
+    cout << this->vao << endl;
+    /*
+        Possibili problemi:
+            - mancano le shader (ma non penso)
+            - non trova la finestra e quindi non fa inizializzare il VAO
+    */
+    glGenVertexArrays(n, &(this->vao));
+    cout << "Pass 1" << endl;
+    glBindVertexArray(this->vao);
 }
 
-void Shape2D::drawShape2D(int n){
+void Shape2D::drawShape2D(int n)
+{
     glGenBuffers(n, &this->vbo_g);
     glBindBuffer(GL_ARRAY_BUFFER, this->vbo_g);
     glBufferData(GL_ARRAY_BUFFER, this->vertex.size() * sizeof(vec3), this->vertex.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
     glEnableVertexAttribArray(0);
 
     glGenBuffers(n, &this->vbo_c);
     glBindBuffer(GL_ARRAY_BUFFER, this->vbo_c);
     glBufferData(GL_ARRAY_BUFFER, this->colors.size() * sizeof(vec4), this->colors.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
     glEnableVertexAttribArray(1);
-
 }
---
+++ Forbidden/SimpleWindow/Utils/Window.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <string.h>
+
+class Window
+{
+private:
+    GLFWwindow *window;
+    int width;
+    int height;
+    const char *windowName;
+
+    void processCloseInput();
+
+public:
+    Window(const char *windowName, int w, int h);
+    int startWindow();
+    void startRender();
+    void startLoop();
+    void closeWindow();
+    char *getWindowName();
+};
